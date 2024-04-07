7305278b1ea60339740c539c9da1b521fd2c3c3a
jon0010 <jonnahuel78@gmail.com>
2024-04-07 02:35:13 -0300

    update upload image handler2

--- src/handlers/postNewProductHandler/postNewProductHandler.ts
+++ src/handlers/postNewProductHandler/postNewProductHandler.ts
@@ -1,43 +1,23 @@
 import { Request, Response } from "express";
-import { v2 as cloudinary } from "cloudinary";
 import Product from "../../models/productModel";
-import multer from "multer";
 import dotenv from "dotenv";
 dotenv.config();
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-const upload = multer();
-
 export const postNewProductHandler = async (req: Request, res: Response) => {
   try {
-    const { name, price, description } = req.body;
+    const { name, price, description, imageUrl } = req.body;
 
-    if (!name || !price || !description) {
+    if (!name || !price || !description || !imageUrl) {
       return res
         .status(400)
         .json({ message: "Todos los campos son requeridos" });
     }
 
-    if (!req.file) {
-      return res.status(400).json({ message: "No se encontró ninguna imagen" });
-    }
-
-    const file = req.file;
-
-    const cloudinaryResponse = await cloudinary.uploader.upload(file.path, {
-      folder: "products",
-    });
-
     const product = new Product({
       name,
       price,
       description,
-      imageUrl: cloudinaryResponse.secure_url,
+      imageUrl,
     });
 
     await product.save();
--- src/handlers/uploadImage/uploadImage.ts
+++ src/handlers/uploadImage/uploadImage.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from "express";
 import { v2 as cloudinary } from "cloudinary";
+import Product from "../../models/productModel";
+import multer from "multer";
+import dotenv from "dotenv";
+dotenv.config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -7,11 +11,11 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+const upload = multer({ dest: "uploads/" }).single("image");
+
 export const uploadImageHandler = async (req: Request, res: Response) => {
   try {
-    console.log("Request Body:", req.body);
-    console.log("Uploaded File:", req.file);
-
+    console.log(req.file);
     if (!req.file) {
       return res.status(400).json({ message: "No se encontró ninguna imagen" });
     }
@@ -19,8 +23,6 @@ export const uploadImageHandler = async (req: Request, res: Response) => {
     const cloudinaryResponse = await cloudinary.uploader.upload(req.file.path, {
       folder: "products",
     });
-
-    console.log("Cloudinary Response:", cloudinaryResponse);
 
     res.status(200).json({ imageUrl: cloudinaryResponse.secure_url });
   } catch (error) {
