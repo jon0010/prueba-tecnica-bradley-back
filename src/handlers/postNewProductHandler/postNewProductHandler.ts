455b8460507bd739a6bc280467a89994da0dac9b
jon0010 <jonnahuel78@gmail.com>
2024-04-07 01:15:16 -0300

    actualizacion de handler para crear producto

--- src/handlers/postNewProductHandler/postNewProductHandler.ts
+++ src/handlers/postNewProductHandler/postNewProductHandler.ts
@@ -1,17 +1,44 @@
 import { Request, Response } from "express";
+import { v2 as cloudinary } from "cloudinary";
 import Product from "../../models/productModel";
+import multer from "multer";
+import dotenv from "dotenv";
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const upload = multer();
 
 export const postNewProductHandler = async (req: Request, res: Response) => {
-  const { name, price, description, imageUrl } = req.body;
-
   try {
-    if (!name || !price || !description || !imageUrl) {
+    const { name, price, description } = req.body;
+
+    if (!name || !price || !description) {
       return res
         .status(400)
-        .json({ message: "Todos los campos son requeridos." });
+        .json({ message: "Todos los campos son requeridos" });
     }
 
-    const product = new Product({ name, price, description, imageUrl });
+    if (!req.file) {
+      return res.status(400).json({ message: "No se encontró ninguna imagen" });
+    }
+
+    const file = req.file;
+
+    const cloudinaryResponse = await cloudinary.uploader.upload(file.path, {
+      folder: "products",
+    });
+
+    const product = new Product({
+      name,
+      price,
+      description,
+      imageUrl: cloudinaryResponse.secure_url,
+    });
 
     await product.save();
 
